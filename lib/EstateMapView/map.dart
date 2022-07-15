7a403f668e93aef98972d37a4d46d03d1e9aee79
Shiva Gautam <shivagautam2054@gmail.com>
2022-07-15 11:50:05 +0545

    SignOut Added

--- lib/EstateMapView/map.dart
+++ lib/EstateMapView/map.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:estate/services/Auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-
 
 class MapScreen extends StatelessWidget {
   const MapScreen({super.key});
@@ -9,32 +10,39 @@ class MapScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance
-        .collection('lands/0IQZxNrmLDwW9Q8mD5Zu/locations')
-        .snapshots(),
-      builder: (context, streamSnapshot) {
-        if (streamSnapshot.connectionState == ConnectionState.waiting){
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-        final documents = streamSnapshot.data?.docs;
-        return ListView.builder(
-          itemCount: documents?.length,
-          itemBuilder: ((context, index) => Container(
-            padding: const EdgeInsets.all(8),
-            child: Text(documents![index]['location']),
-          )),
-        );
-      }
-      )
+      body: Center(
+        child: Column(children: [
+          ElevatedButton(
+            onPressed: () {
+              AuthService().signOut();
+            },
+            child: const Text('SignOut'),
+          )
+        ]),
+      ),
     );
-    
-    // Container(
-    //   child: Center(
-    //     child: Text("Map View"),
-    //   ),
+
+    // return Scaffold(
+    //   body: StreamBuilder(
+    //     stream: FirebaseFirestore.instance
+    //     .collection('lands/0IQZxNrmLDwW9Q8mD5Zu/locations')
+    //     .snapshots(),
+    //   builder: (context, streamSnapshot) {
+    //     if (streamSnapshot.connectionState == ConnectionState.waiting){
+    //       return const Center(
+    //         child: CircularProgressIndicator(),
+    //       );
+    //     }
+    //     final documents = streamSnapshot.data?.docs;
+    //     return ListView.builder(
+    //       itemCount: documents?.length,
+    //       itemBuilder: ((context, index) => Container(
+    //         padding: const EdgeInsets.all(8),
+    //         child: Text(documents![index]['location']),
+    //       )),
+    //     );
+    //   }
+    //   )
     // );
   }
 }
