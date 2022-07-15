ae093c16e1de05dfc103dc84a628d099071f9b77
Shiva Gautam <shivagautam2054@gmail.com>
2022-07-15 10:24:25 +0545

    Map Change

--- lib/EstateMapView/map.dart
+++ lib/EstateMapView/map.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 
 class MapScreen extends StatelessWidget {
   const MapScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("Map View"),
-      ),
+    return Scaffold(
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+        .collection('lands/0IQZxNrmLDwW9Q8mD5Zu/locations')
+        .snapshots(),
+      builder: (context, streamSnapshot) {
+        if (streamSnapshot.connectionState == ConnectionState.waiting){
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        final documents = streamSnapshot.data?.docs;
+        return ListView.builder(
+          itemCount: documents?.length,
+          itemBuilder: ((context, index) => Container(
+            padding: const EdgeInsets.all(8),
+            child: Text(documents![index]['location']),
+          )),
+        );
+      }
+      )
     );
+    
+    // Container(
+    //   child: Center(
+    //     child: Text("Map View"),
+    //   ),
+    // );
   }
 }
