1e580e6326c16d5878da955f767093fc666a6754
Shiva Gautam <shivagautam2054@.com>
2022-07-19 09:37:04 +0545

    List Updated and  Change

--- lib/EstateList/list.dart
+++ lib/EstateList/list.dart
@@ -1,13 +1,31 @@
 import 'package:flutter/material.dart';
+import './dummy_data.dart';
+import 'category_item.dart';
 
 class ListScreen extends StatelessWidget {
   const ListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("List"),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('List View'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES
+            .map((catData) => CategoryItem(
+                  id: catData.id,
+                  title: catData.title,
+                  color: catData.color,
+                ))
+            .toList(),
       ),
     );
   }
